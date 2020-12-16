c464b56b408ac5fd5c1f392eec9cb7c2f145a47d
hyunjae <steveysung91@gmail.com>
2020-12-16 17:47:12 +0900

    ARCHITECTURE OF NESTJS 1.2-2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 // 데코레이터
 // 데코레이터는 클래스에 함수 기능을 추가할 수 있다
@@ -8,7 +6,7 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController], // express의 router 같은 것
-  providers: [AppService],
+  controllers: [], // express의 router 같은 것
+  providers: [],
 })
 export class AppModule {} // 루트 모듈 같은 개념
