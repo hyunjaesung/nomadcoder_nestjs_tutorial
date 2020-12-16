043220339e3dd5750806355a46291331e4b13efe
hyunjae <steveysung91@gmail.com>
2020-12-16 16:44:07 +0900

    ARCHITECTURE OF NESTJS 1.1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,14 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get() // Get Router랑 같은 역할
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/hello') // /hello 주소 라우팅 hello 주소 접근하면 sayHello() 실행
+  // 주의점은 데코레이터와 클래스 붙어있어야한다
+  sayHello(): string {
+    return 'Hello everyone!!';
   }
 }
